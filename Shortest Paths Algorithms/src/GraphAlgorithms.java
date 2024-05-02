83db9893da528e622e0ad278b0c2afedfae118af
Karene Antoine Nassif <antoinekarene@gmail.com>
2024-05-02 19:43:10 +0300

    Bellman Ford & read from file done

--- Shortest Paths Algorithms/src/GraphAlgorithms.java
+++ Shortest Paths Algorithms/src/GraphAlgorithms.java
@@ -1,5 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class GraphAlgorithms implements IGraphAlg{
     private String filePath;
+
+
+//    public int[][] getGraph() {
+//        return graph;
+//    }
+//
+//    public void setGraph(int[][] graph) {
+//        this.graph = graph;
+//    }
+
     private int[][] graph;
     public GraphAlgorithms(String filePath){
         this.filePath = filePath;
@@ -7,10 +22,38 @@ public class GraphAlgorithms implements IGraphAlg{
 
     public void readGraph(){
         // Read the graph from the file
+        try {
+            File file = new File(filePath);
+            Scanner scanner = new Scanner(file);
+
+            // Read number of vertices and edges
+            int V = scanner.nextInt();
+            int E = scanner.nextInt();
+
+            // Initialize graph with 0 weight for all edges
+            graph = new int[V][V];
+
+            // Read edges
+            for (int i = 0; i < E; i++) {
+                int source = scanner.nextInt();
+                int destination = scanner.nextInt();
+                int weight = scanner.nextInt();
+
+                // Update graph with edge weight
+                graph[source][destination] = weight;
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
     }
     @Override
     public int size() {
-        return 0;
+        return graph.length;
     }
 
     @Override
@@ -20,7 +63,31 @@ public class GraphAlgorithms implements IGraphAlg{
 
     @Override
     public boolean bellmanFord(int source, int[] cost, int[] parent) {
-        return false;
+        //return true if there is no negative cycle
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        cost[source] = 0;
+        for (int i = 0; i < size() - 1; i++) {
+            for (int u = 0; u < size(); u++) {
+                for (int v = 0; v < size(); v++) {
+                    if (graph[u][v] != 0) {
+                        if (cost[v] > cost[u] + graph[u][v]) {
+                            cost[v] = cost[u] + graph[u][v];
+                            parent[v] = u;
+                        }
+                    }
+                }
+            }
+        }
+        for (int u = 0; u < size(); u++) {
+            for (int v = 0; v < size(); v++) {
+                if (graph[u][v] != 0) {
+                    if (cost[v] > cost[u] + graph[u][v]) {
+                        return false ;
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     @Override
