8a952af1b0aed20a18b917ea35ca7c731607f13d
AndrewAchraf <andrewachraf02@gmail.com>
2024-05-07 23:58:21 +0300

    Added special Case Tests

--- Shortest Paths Algorithms/src/GraphAlgorithmsTest.java
+++ Shortest Paths Algorithms/src/GraphAlgorithmsTest.java
@@ -179,23 +179,6 @@ class GraphAlgorithmsTest {
         System.out.println("All Pair Bellman-Ford took on average:  " + format(totalTimeBellmanFordAllPair / samples));
         System.out.println("All Pair Floyd-Warshall took on average:  " + format(totalTimeFloydWarshallAllPair / samples));
     }
-    @Test
-    void fileNotFound() {
-        assertThrows(IOException.class, () -> new GraphAlgorithms("/dummy/path.txt"));
-    }
-    @Test
-    void emptyFile() {
-        assertThrows(IOException.class, () -> new GraphAlgorithms("C:\\Users\\Lenovo\\Desktop\\Shortest-Paths-Algorithms\\empty.txt"));
-    }
-    @Test
-    void testSize() throws IOException {
-        GraphAlgorithms graph = new GraphAlgorithms("C:\\Users\\Lenovo\\Desktop\\Shortest-Paths-Algorithms\\empty_graph.txt");
-        assertEquals(0, graph.size());
-        graph = new GraphAlgorithms("C:\\Users\\Lenovo\\Desktop\\Shortest-Paths-Algorithms\\size_test.txt");
-        assertEquals(5, graph.size());
-    }
-
-
 
     static String format(long nanoseconds){
         if (nanoseconds < 1000) return String.format("%d ns", nanoseconds);
@@ -211,7 +194,198 @@ class GraphAlgorithmsTest {
         return "E is O(V)";
     }
 
+    /***Special Cases***/
+    @Test
+    void fileNotFound() {
+        assertThrows(IOException.class, () -> new GraphAlgorithms("/dummy/path.txt"));
+    }
+    @Test
+    void emptyFile() {
+        assertThrows(IOException.class, () -> new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\empty.txt"));
+    }
+    @Test
+    void testSize() throws IOException {
+        GraphAlgorithms graph = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\empty_graph.txt");
+        assertEquals(0, graph.size());
+        graph = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\size_test.txt");
+        assertEquals(5, graph.size());
+    }
+    @Test
+    void testNonExistentSource() throws IOException {
+        /*Test Algorithm With a source node that doesn't exist*/
+        GraphAlgorithms graph = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\dijkestra.txt");
+        int[] cost = new int[graph.size()];
+        int[] parent = new int[graph.size()];
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> graph.dijkestra(6, cost, parent));
+    }
+    @Test
+    void testNonExistentDestination() throws IOException {
+        /*Test Algorithm With a source node that doesn't exist*/
+        GraphAlgorithms graph = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\dijkestra.txt");
+        int[] cost = new int[graph.size()];
+        int[] parent = new int[graph.size()];
+        graph.dijkestra(0, cost, parent);
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> System.out.println(cost[6]));
+    }
+    @Test
+    void testNonExistentPath() throws IOException {
+        GraphAlgorithms graph = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\dijkestra.txt");
+        int[] cost = new int[graph.size()];
+        int[] parent = new int[graph.size()];
+        graph.dijkestra(2, cost, parent);
+        assertEquals(Integer.MAX_VALUE, cost[1]);
+        assertEquals(-1, parent[1]);
+    }
+    @Test
+    void testNormalDijkstra() throws IOException {
+        GraphAlgorithms graph = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\dijkestra.txt");
+        int[][] cost = new int[graph.size()][graph.size()];
+        int[][] parent = new int[graph.size()][graph.size()];
+        graph.cost_between_allpairs_with_dijkestra(cost, parent);
+        /*Test Normal Algorithm Run*/
+        int[][] expectedCost = new int[][]{
+                {0, 2, 3, 8, 6, 9},
+                {Integer.MAX_VALUE, 0, 1, 6, 4, 7},
+                {Integer.MAX_VALUE, Integer.MAX_VALUE, 0, 5, 3, 6},
+                {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 0, Integer.MAX_VALUE, 1},
+                {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 2, 0, 3},
+                {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 0}
+        };
+        assertArrayEquals(expectedCost, cost);
+        int[][] expectedParents = new int[][]{
+                {0, 0, 1, 4, 2, 3},
+                {-1, 1, 1, 4, 2, 3},
+                {-1, -1, 2, 4, 2, 3},
+                {-1, -1, -1, 3, -1, 3},
+                {-1, -1, -1, 4, 4, 3},
+                {-1, -1, -1, -1, -1, 5}
+        };
+        assertArrayEquals(expectedParents, parent);
+    }
+    @Test
+    void testNormalBellmanFord() throws IOException {
+        GraphAlgorithms graph = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\bellman.txt");
+        int[] cost = new int[graph.size()];
+        int[] parent = new int[graph.size()];
+        graph.bellmanFord(2, cost, parent);
+        /*Test Normal Algorithm Run*/
+        assertArrayEquals(new int[]{Integer.MAX_VALUE, -2, 0, Integer.MAX_VALUE, -3, Integer.MAX_VALUE, 0}, cost);
+        assertArrayEquals(new int[]{-1, 2, 2, -1, 1, -1, 4}, parent);
+    }
+    @Test
+    void testNormalFloydWarshall() throws IOException {
+        GraphAlgorithms graph = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\floyd.txt");
+        int[][] cost = new int[graph.size()][graph.size()];
+        int[][] parent = new int[graph.size()][graph.size()];
+        graph.floydWarshall(cost, parent);
+        /*Test Normal Algorithm Run*/
+        int[][] expectedCost = new int[][]{
+                {0, 3, 5, 6},
+                {5, 0, 2, 3},
+                {3, 6, 0, 1},
+                {2, 5, 7, 0},
+        };
+        assertArrayEquals(expectedCost, cost);
+    }
+    @Test
+    void testDijkstraWithNegativeWeights() throws IOException {
+        GraphAlgorithms graph = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\bellman.txt");
+        int[] cost = new int[graph.size()];
+        int[] parent = new int[graph.size()];
+        graph.dijkestra(0, cost, parent);
+        /*Test Algorithm With Negative Weights*/
+        /*The algorithm should fail because it's not designed to handle negative weights*/
+        assertEquals(3, cost[6]);
+    }
+    @Test
+    void testBellmanFordWithNegativeWeight() throws IOException {
+        GraphAlgorithms graph = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\bellman.txt");
+        int[] cost = new int[graph.size()];
+        int[] parent = new int[graph.size()];
+        graph.bellmanFord(0, cost, parent);
+        /*Test Algorithm With Negative Weights*/
+        assertEquals(3, cost[6]);
+    }
+    @Test
+    void testFloydWarshallWithNegativeWeight() throws IOException {
+        GraphAlgorithms graph = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\bellman.txt");
+        int[][] cost = new int[graph.size()][graph.size()];
+        int[][] parent = new int[graph.size()][graph.size()];
+        graph.floydWarshall(cost, parent);
+        /*Test Algorithm With Negative Weights*/
+        assertEquals(3, cost[0][6]);
+    }
+    @Test
+    void testBellmanFordWithNegativeCycle() throws IOException {
+        GraphAlgorithms actuallyNoCycle = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\negativeCycleDoesntExist.txt");
+        int[] cost = new int[actuallyNoCycle.size()];
+        int[] parent = new int[actuallyNoCycle.size()];
 
+        /*Passes because there is no negative cycle*/
+        assertTrue(actuallyNoCycle.bellmanFord(0, cost, parent));
+        GraphAlgorithms negativeCycleWithPathFromSource = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\negativeCycleWithPathFromSource.txt");
+        cost = new int[negativeCycleWithPathFromSource.size()];
+        parent = new int[negativeCycleWithPathFromSource.size()];
 
+        /*Passes because there is a negative cycle reachable from the source*/
+        assertFalse(negativeCycleWithPathFromSource.bellmanFord(0, cost, parent));
+        GraphAlgorithms negativeCycleWithNoPathFromSource = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\negativeCycleWithNoPathFromSource.txt");
+        cost = new int[negativeCycleWithNoPathFromSource.size()];
+        parent = new int[negativeCycleWithNoPathFromSource.size()];
+        /*Fails because there is a negative cycle but it's not reachable from the source*/
+        assertFalse(negativeCycleWithNoPathFromSource.bellmanFord(0, cost, parent));
+    }
+    @Test
+    void testFloydWarshallWithNegativeCycle() throws IOException {
+        GraphAlgorithms actuallyNoCycle = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\negativeCycleDoesntExist.txt");
+        int[][] cost = new int[actuallyNoCycle.size()][actuallyNoCycle.size()];
+        int[][] parent = new int[actuallyNoCycle.size()][actuallyNoCycle.size()];
+        /*Passes because there is no negative cycle*/
+        assertTrue(actuallyNoCycle.floydWarshall(cost, parent));
+        GraphAlgorithms negativeCycleWithPathFromSource = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\negativeCycleWithPathFromSource.txt");
+        cost = new int[negativeCycleWithPathFromSource.size()][negativeCycleWithPathFromSource.size()];
+        parent = new int[negativeCycleWithPathFromSource.size()][negativeCycleWithPathFromSource.size()];
 
+        /*Passes because there is a negative cycle reachable from the source*/
+        assertFalse(negativeCycleWithPathFromSource.floydWarshall(cost, parent));
+        GraphAlgorithms negativeCycleWithNoPathFromSource = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\negativeCycleWithNoPathFromSource.txt");
+        cost = new int[negativeCycleWithNoPathFromSource.size()][negativeCycleWithNoPathFromSource.size()];
+        parent = new int[negativeCycleWithNoPathFromSource.size()][negativeCycleWithNoPathFromSource.size()];
+        /**This one passes this time because there is a negative cycle even if it's not reachable from the source**/
+        assertFalse(negativeCycleWithNoPathFromSource.floydWarshall(cost, parent));
+    }
+    @Test
+    void graphWithOneNode() throws IOException {
+        GraphAlgorithms graph = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\graphWithOneNode.txt");
+        int[] cost = new int[graph.size()];
+        int[] parent = new int[graph.size()];
+        graph.dijkestra(0, cost, parent);
+        assertArrayEquals(new int[]{0}, cost);
+        assertArrayEquals(new int[]{0}, parent);
+    }
+    @Test
+    void testTreeGraph() throws IOException {
+        GraphAlgorithms graph = new GraphAlgorithms("D:\\Koleya\\CSED Year 2 (2023-2024)\\2nd Semester - Spring 2024\\DS II\\Labs\\Lab 3\\Shortest-Paths-Algorithms\\treeGraph.txt");
+        int[][] cost = new int[graph.size()][graph.size()];
+        int[][] parent = new int[graph.size()][graph.size()];
+        graph.cost_between_allpairs_with_dijkestra(cost, parent);
+        int[][] expectedCost = new int[][]{
+                {0, 2, 3, 5, 8, 6},
+                {Integer.MAX_VALUE, 0, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE},
+                {Integer.MAX_VALUE, Integer.MAX_VALUE, 0, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE},
+                {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 0, 3, 1},
+                {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 0, Integer.MAX_VALUE},
+                {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 0}
+        };
+        assertArrayEquals(expectedCost, cost);
+//        int[][] expectedParents = new int[][]{
+//                {0, 0, 1, 4, 2, 3},
+//                {-1, 1, 1, 4, 2, 3},
+//                {-1, -1, 2, 4, 2, 3},
+//                {-1, -1, -1, 3, -1, 3},
+//                {-1, -1, -1, 4, 4, 3},
+//                {-1, -1, -1, -1, -1, 5}
+//        };
+//        assertArrayEquals(expectedParents, parent);
+    }
 }
